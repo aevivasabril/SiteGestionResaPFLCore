a7ed9cf55ba453453dcbda96f59642f6d21d8e06
VIVAS <anny.vivas@inra.fr>
2020-11-23 14:52:04 +0100

    Ajout du controleur chargé de traiter les actions de validation ou refus des réservations ( actions Post réservations traitées par les admin logistic). Ajout de la vue menu consultation. Ajout du chemin référence pour accèder depuis le ménu "Equipe" => réservations

---
+++ SiteGestionResaCore/SiteGestionResaCore/Areas/Reservation/Controllers/ValidationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SiteGestionResaCore.Data;
+using SiteGestionResaCore.Services;
+
+namespace SiteGestionResaCore.Areas.Reservation.Controllers
+{
+    [Area("Reservation")]
+    public class ValidationController : Controller
+    {
+        private readonly UserManager<utilisateur> userManager;
+        private readonly IEmailSender emailSender;
+
+        public ValidationController(
+            UserManager<utilisateur> userManager,
+            IEmailSender emailSender)
+        {
+            this.userManager = userManager;
+            this.emailSender = emailSender;
+        }
+        public IActionResult MenuConsultation()
+        {
+            return View();
+        }
+    }
+}
