0c688ab718ddac77b81eaaa414051a776e942f00
VIVAS <anny.vivas@inra.fr>
2020-11-26 16:04:48 +0100

    Table avec les infos sans bords et remplie correctement avec les infos de chaque essai.

--- SiteGestionResaCore/SiteGestionResaCore/Areas/Reservation/Data/Validation/InfosEssai.cs
+++ SiteGestionResaCore/SiteGestionResaCore/Areas/Reservation/Data/Validation/InfosEssai.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,17 +8,29 @@ namespace SiteGestionResaCore.Areas.Reservation.Data.Validation
 {
     public class InfosEssai
     {
+        [DisplayName("N° Essai")]
         public int id { get; set; }
+        [DisplayName("Date Création")]
         public DateTime DateCreation { get; set; }
+        [DisplayName("Email manipulateur essai")]
         public string MailManipulateur { get; set; }
+        [DisplayName("Type de produit entrant")]
         public string TypeProduitEntrant { get; set; }
+        [DisplayName("Demande saisie par")]
         public string MailUser { get; set; }
+        [DisplayName("Descriptif essai")]
         public string Commentaire { get; set; }
+        [DisplayName("Confidentialité essai")]
         public string Confidentialite { get; set; }
+        [DisplayName("Transport assuré par")]
         public bool TransportStlo { get; set; }
+        [DisplayName("Précision produit")]
         public string PrecisionProd { get; set; }
+        [DisplayName("Quantité produit")]
         public string QuantiteProd { get; set; }
+        [DisplayName("Provenance produit")]
         public string ProveProd { get; set; }
+        [DisplayName("Destination produit")]
         public string DestProd { get; set; }
     }
 }
