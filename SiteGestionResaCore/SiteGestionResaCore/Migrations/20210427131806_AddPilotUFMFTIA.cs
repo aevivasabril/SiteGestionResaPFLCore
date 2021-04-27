065017ab33084805768c2c9835c5b8207866380f
VIVAS <anny.vivas@inra.fr>
2021-04-27 15:26:36 +0200

    Ajouter le nom de la table PcVue pour l'équipement "PILOT0002" UF/MF TIA (JY)

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20210427131806_AddPilotUFMFTIA.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class AddPilotUFMFTIA : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "e9f8887a-a747-4973-a57a-986af6146a60");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d0b0ed54-c3b8-4f4b-9997-a2166351cfa0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "be84df92-e33b-4225-9f36-9785f7a5c95d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "0b145b42-f9a7-43c4-914d-4dd1c5c87a0d");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 214,
+                column: "nomTabPcVue",
+                value: "tab_UA_UFMF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "82582b6b-88b4-4289-a7cc-795c9b2d509a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d7d6ddab-466c-49ce-be2e-d1e58291d040");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "31432d8d-36f7-4605-93c5-e1b45accbaa5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "ee472420-197f-4b6a-b26a-9e817be936e4");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 214,
+                column: "nomTabPcVue",
+                value: null);
+        }
+    }
+}
