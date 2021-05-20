e5626a9a49961e8501dc436e7e068619dce4a8db
VIVAS <anny.vivas@inra.fr>
2021-05-20 15:35:47 +0200

    J'ai mis des cases pour 2 des rubriques (les seules dont l'utilisateur pourrait ne pas être concerné) pour les futures statistiques. si les notes sont inférieures à 3 que le commentaire est null et dans certains cas que la case n'est pas cochée alors demander à la personne de justifier sa note dans l'espace prevu pour les commentaires. Suppression de la rubrique "Main d'oeuvre" Amélioration des questions enquête + ajout des nouvelles questions

--- SiteGestionResaCore/SiteGestionResaCore/Areas/Enquete/Controllers/EnqueteController.cs
+++ SiteGestionResaCore/SiteGestionResaCore/Areas/Enquete/Controllers/EnqueteController.cs
@@ -63,6 +63,7 @@ namespace SiteGestionResaCore.Areas.Enquete.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult EnvoyerEnquete(EnqueteViewModel vm, int id)
         {
+            bool PointBas = false; // Boolean pour indiquer si un des points des rubriques est inferieur à 3 (renvoie vers ENDT)
             // requete pour obtenir juste le commentaire pour la rubrique 1: -1(.*?)1         1113-1hola la cosa esta aqui1-2223-
             // REGEX pour obtenir les contenus dans les balises: <1Mcomm>(.*?)<\/1Mcomm>
 
@@ -73,14 +74,52 @@ namespace SiteGestionResaCore.Areas.Enquete.Controllers
             // < 5M2 > 2 </ 5M2 >< 5M3 > 3 </ 5M3 >< 5Mcomm > Commentaire </ 5mcomm >
             // Format pour sauvegarder les réponses aux enquêtes: N°Rubrique
 
+            // Si la personne coche la case "non concerné" pour une des rubriques alors enlever l'erreur
+            if (vm.IsNotConcerneMaintMat)
+                ModelState.Remove("MaintenanceMat");
+            if (vm.IsNotConcerneApproviMat)
+                ModelState.Remove("ApprovisioMatiere");
+
+            #region Détecter les notes bases pour chaque rubrique pour demander de les justifier
+
+            // Vérification Rubrique 1
+            if ((vm.LogicielMeth < 3 || vm.AccessibiliteMeth < 3) && vm.CommentMéthodes == null)
+            {
+                ModelState.AddModelError("CommentMéthodes", "Merci d'ajouter un commentaire avec la cause suite à votre appreciation pas satisfaisant");
+                PointBas = true;
+            }
+            // Vérification Rubrique 2
+            if ((vm.DisponibiliteMat < 3 || vm.OperationnabiliteMat < 3 || (vm.MaintenanceMat < 3 && vm.IsNotConcerneMaintMat == false)) && vm.CommentMateriels == null)
+            {
+                ModelState.AddModelError("CommentMateriels", "Merci d'ajouter un commentaire avec la cause suite à votre appreciation pas satisfacente");
+                PointBas = true;
+            }
+            // Vérification Rubrique 3
+            if ((vm.HygieneMil < 3 || vm.ConfidentMil < 3 || vm.SecureMil < 3 || vm.MaterielMil < 3 || vm.EnergieMil < 3) && vm.CommentaireMil == null)
+            {
+                ModelState.AddModelError("CommentMateriels", "Merci d'ajouter un commentaire avec la cause suite à votre appreciation pas satisfacente");
+                PointBas = true;
+            }
+            if (( (vm.ApprovisioMatiere < 3 && vm.IsNotConcerneApproviMat == false) || vm.NettoMatiere < 3 || vm.MaterielMatiere < 3) && vm.CommentaireMatiere == null)
+            {
+                ModelState.AddModelError("CommentaireMatiere", "Merci d'ajouter un commentaire avec la cause suite à votre appreciation pas satisfacente");
+                PointBas = true;
+            }
+
+            if (PointBas)
+            {
+                goto ENDT;
+            }
+            #endregion
+
             string ReponseBalise;
             if (ModelState.IsValid) // Ajouter le créneau de réservation dans la liste
             {
+
                 ReponseBalise = "<1M1>" + vm.LogicielMeth + "</1M1><1M2>" + vm.AccessibiliteMeth + "</1M2><1Mcomm>" + vm.CommentMéthodes +
-                "</1Mcomm><2M1>" + vm.DisponibiliteMat + "</2M1><2M2>" + vm.OperationnabiliteMat + "</2M2><2M3>" + vm.MaintenanceMat + "</2M3><2Mcomm>" + vm.CommentMateriels +
-                "</2Mcomm><3M1>" + vm.DisponibiliteMain + "</3M1><3M2>" + vm.CompetenceMain + "</3M2 ><3M3>" + vm.FormationMain + "</3M3><3M3bit>" + vm.IsNotConcerneFormationMain + "</3M3bit><3M4>" + vm.ReactiviteMain + "</3M4><3Mcomm>" + vm.CommentaireMain +
-                "</3Mcomm><4M1>" + vm.HygieneMil + "</4M1><4M2>" + vm.ConfidentMil + "</4M2><4M3>" + vm.EspaceMil + "</4M3><4Mcomm>" + vm.CommentaireMil +
-                "</4Mcomm><5M1>" + vm.ApprovisioMatiere + "</5M1><5M2>" + vm.EnergieMatiere + "</5M2><5M3>" + vm.MaterielMatiere + "</5M3><5Mcomm>" + vm.CommentaireMatiere + "</5Mcomm>";
+                "</1Mcomm><2M1>" + vm.DisponibiliteMat + "</2M1><2M2>" + vm.OperationnabiliteMat + "</2M2><2M3>" + vm.MaintenanceMat + "</2M3><2M3bit>" + vm.IsNotConcerneMaintMat + "</ 2M3bit ><2Mcomm>" + vm.CommentMateriels +
+                "</2Mcomm><3M1>" + vm.HygieneMil + "</3M1><3M2>" + vm.ConfidentMil + "</3M2 ><3M3>" + vm.SecureMil + "</3M3><3M4>" + vm.MaterielMil + "</3M4><3M5>" + vm.EnergieMil + "</3M5><3Mcomm>" + vm.CommentaireMil +
+                "</3Mcomm><4M1>" + vm.ApprovisioMatiere + "</4M1><4M1bit>"+ vm.IsNotConcerneApproviMat+"</4M1bit><4M2>" + vm.NettoMatiere + "</4M2><4M3>" + vm.MaterielMatiere + "</4M3><4Mcomm>" + vm.CommentaireMatiere + "</4Mcomm>";
 
                 enquete enquete = EnqueteDb.ObtenirEnqueteFromEssai(id);
                 // Enregistrer les résultats de l'enquete
@@ -91,6 +130,9 @@ namespace SiteGestionResaCore.Areas.Enquete.Controllers
             }
             else
             {
+                goto ENDT;
+            }
+            ENDT:
                 // Recharger mon model view 
                 // Obtenir l'essai à partir de l'ID enquete
                 essai ess = EnqueteDb.ObtenirEssai(id);
@@ -104,7 +146,6 @@ namespace SiteGestionResaCore.Areas.Enquete.Controllers
                 // Renvoyer vers la vue enquete pour remplir l'enquete
                 return View("EnqueteSatisfaction", vm);
                 //return RedirectToAction("EnqueteSatisfaction", "Enquete", new { area = "Enquete", id= 5 });
-            }
         }
     }
 }
--- SiteGestionResaCore/SiteGestionResaCore/Areas/Enquete/Data/EnqueteViewModel.cs
+++ SiteGestionResaCore/SiteGestionResaCore/Areas/Enquete/Data/EnqueteViewModel.cs
@@ -15,94 +15,121 @@ namespace SiteGestionResaCore.Areas.Enquete.Data
         public string NumProjet { get; set; }
         public string TitreProj { get; set; }
 
-        #region Méthodes
+        #region 1 Méthodes
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Logiciel de réservation: ")]
+        //[Display(Name = "Logiciel de réservation: ")]
+        [Display(Name = "Est-ce que le logiciel de réservation satisfait vos attentes (réservation des pilotes, récupération des données,...)?: ")]
         public int LogicielMeth { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Accesibilité/Clarté des procédures (équipements, nettoyage/désinfection, accès à la PFL,...: ")]
+        //[Display(Name = "Accesibilité/Clarté des procédures (équipements, nettoyage/désinfection, accès à la PFL,...: ")]
+        [Display(Name = "Les procédures relatives aux modalités d'utilisation de la PFL, à l'utilisation des materiels, utilisation nettoyage étaient-elles claires?")]
         public int AccessibiliteMeth { get; set; }
 
         [Display(Name = "Commentaire: ")]
         public string CommentMéthodes { get; set; }
         #endregion
 
-        #region Matériels (outils de travail)
+        #region 2 Matériels (outils de travail)
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Disponibilité: ")]
+        //[Display(Name = "Disponibilité: ")]
+        [Display(Name = "Est-ce que les matériels sont disponibles facilement?: ")]
         public int DisponibiliteMat { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Opérationnabilité: ")]
+        //[Display(Name = "Opérationnabilité: ")]
+        [Display(Name = "Les matériels sont-ils en bon état de fonctionnement et de propreté?: ")]
         public int OperationnabiliteMat { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Maintenance: ")]
+        [Display(Name = "L'assistance technique a-t-elle été satisfaisante?: ")]
         public int MaintenanceMat { get; set; }
+
+        //[Required(ErrorMessage = "Champ requis")]
+        [Display(Name = "Cochez cette case si vous n'êtes pas concerné par la rubrique")]
+        public bool IsNotConcerneMaintMat { get; set; }
 
         [Display(Name = "Commentaire: ")]
         public string CommentMateriels { get; set; }
         #endregion
 
-        #region Main d'oeuvre
+        #region 3 Main d'oeuvre
 
-        [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Disponibilité: ")]
+        /*[Range(1, 4, ErrorMessage = "Note requise")]
+        //[Display(Name = "Disponibilité: ")]
+        [Display(Name = "Le personnel PFL a été disponible pour vous informer et vous aider à la préparation et réalisation des essais?: ")]
         public int DisponibiliteMain { get; set; }
 
+        // TODO: A SUPPRIMER
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Compétence: ")]
+        [Display(Name = " SUPPRIMER Compétence: ")]
         public int CompetenceMain { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Formation/Tutorat: ")]
+        //[Display(Name = "Formation/Tutorat: ")]
+        [Display(Name = "A SUPPRIMER Pour les nouveaux utilisateurs: Comment jugez vous la formation reçue?")]
         public int FormationMain { get; set; }
 
         //[Required(ErrorMessage = "Champ requis")]
-        [Display(Name = "Cochez cette case si vous n'êtes pas concerné par la Formation/Tutorat! ")]
+        [Display(Name = "Cochez cette case si vous n'êtes pas concerné par la rubrique")]
         public bool IsNotConcerneFormationMain { get; set; }
 
+        // TODO: A SUPPRIMER
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Reactivité: ")]
+        [Display(Name = "SUPPRIMER Le personnel PFL était-il reactif à des problèmes que vous avez pu rencontrer (ex: panne d'un équipement)? ")]
         public int ReactiviteMain { get; set; }
 
         [Display(Name = "Commentaire: ")]
-        public string CommentaireMain { get; set; }
+        public string CommentaireMain { get; set; }*/
+
         #endregion
 
-        #region Milieu
+        #region 3 Milieu
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Hygiène/Securité/Ergonomie/Réglementation: ")]
+        //[Display(Name = "Hygiène/Securité/Ergonomie/Réglementation: ")]
+        [Display(Name = "L'état de proprète était-il satisfaisant?: ")]
         public int HygieneMil { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Confidentialité: ")]
+        //[Display(Name = "Confidentialité: ")]
+        [Display(Name = "La confidentialité de vos essais était-elle assurée?: ")]
         public int ConfidentMil { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Espace de travail: ")]
-        public int EspaceMil { get; set; }
+        [Display(Name = "Est-ce que votre environnement de travail était-il secure?: ")]
+        public int SecureMil { get; set; }
+
+        [Range(1, 4, ErrorMessage = "Note requise")]
+        [Display(Name = "Le petit materiel de la zone de travail était-il disponible?: ")]
+        public int MaterielMil { get; set; }
+
+        [Range(1, 4, ErrorMessage = "Note requise")]
+        [Display(Name = "Comment jugez vous le fonctionnement des énergies et fluides au cours de votre essai?: ")]
+        public int EnergieMil { get; set; }
 
         [Display(Name = "Commentaire: ")]
         public string CommentaireMil { get; set; }
+
         #endregion
 
-        #region Matière
-
+        #region 4 Matière
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Approvisionnement (lait produits laitières et produits de nettoyage): ")]
+        [Display(Name = "L'approvisionnement de matière première était-il satisfaisant?: ")]
         public int ApprovisioMatiere { get; set; }
 
-        [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Energie et fluides: ")]
-        public int EnergieMatiere { get; set; }
+        //[Required(ErrorMessage = "Champ requis")]
+        [Display(Name = "Cochez cette case si vous n'êtes pas concerné par la rubrique")]
+        public bool IsNotConcerneApproviMat { get; set; }
 
         [Range(1, 4, ErrorMessage = "Note requise")]
-        [Display(Name = "Petit matériel et consommable (essuis main, charlotte, surchaussures,...: ")]
+        [Display(Name = "Disponibilité des produits de nettoyage?: ")]
+        public int NettoMatiere { get; set; }
+
+        [Range(1, 4, ErrorMessage = "Note requise")]
+        [Display(Name = "L'approvisionnement des consommables(essuis main, surchaussures,...) était-il satisfaisant?: ")]
         public int MaterielMatiere { get; set; }
 
         [Display(Name = "Commentaire: ")]
