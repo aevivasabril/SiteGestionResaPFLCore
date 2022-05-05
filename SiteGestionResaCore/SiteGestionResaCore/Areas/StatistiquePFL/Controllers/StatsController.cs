5c70665e5154aed9ca07e472e1a079e782ed2c78
VIVAS <anny.vivas@inra.fr>
2022-05-05 14:03:19 +0200

    Incomplet.

---
+++ SiteGestionResaCore/SiteGestionResaCore/Areas/StatistiquePFL/Controllers/StatsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SiteGestionResaCore.Areas.StatistiquePFL.Data;
+
+namespace SiteGestionResaCore.Areas.StatistiquePFL.Controllers
+{
+    [Area("StatistiquePFL")]
+    public class StatsController : Controller
+    {
+        public IActionResult AccueilStats()
+        {
+            AccueilStatsVM vm = new AccueilStatsVM();
+            return View("AccueilStats", vm);
+        }
+
+        [HttpPost]
+        public IActionResult GenererExcelResas(AccueilStatsVM vm)
+        {
+            StringBuilder csv = new StringBuilder();
+            string titreCsv = null;
+
+            if (vm.DateAu != null && vm.DateDu != null) // Vérification uniquement des datePicker pour l'affichage du calendrier
+            {
+                if (vm.DateDu.Value <= vm.DateAu.Value)
+                {
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "La date fin pour l'affichage du planning équipement ne peut pas être inférieure à la date début");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Oups! Vous avez oublié de saisir les dates! ");
+            }
+
+            return View("AccueilStats", vm);
+        }
+    }
+}
---
+++ SiteGestionResaCore/SiteGestionResaCore/Areas/StatistiquePFL/Data/InfosReservations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteGestionResaCore.Areas.StatistiquePFL.Data
+{
+    public class InfosReservations
+    {
+        public string NumProjet { get; set; }
+        public string TitreProjet { get; set; }
+        public string RespProjet { get; set; }
+        public string TypeProjet { get; set; }
+        public string TitreEssai { get; set; }
+        public string NomEquipement { get; set; }
+        public int IdEssai { get; set; }
+        public string NomOrganisme { get; set; }
+        public string NomEquipe { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateDebutResa { get; set; }
+        public DateTime DateFinResa { get; set; }
+
+
+    }
+}
