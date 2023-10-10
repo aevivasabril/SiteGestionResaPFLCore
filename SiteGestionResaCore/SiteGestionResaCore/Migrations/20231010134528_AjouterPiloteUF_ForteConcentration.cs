8ab3698ecd04465541e0e252cbe200814b3c1155
VIVAS <anny.vivas@inra.fr>
2023-10-10 15:56:39 +0200

    Ajouter dans la BDD le nvo pilote UF forte concentration installé cet été par TIA

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20231010134528_AjouterPiloteUF_ForteConcentration.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class AjouterPiloteUF_ForteConcentration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "146e0bd4-1099-46ef-97b6-59143850557a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "62850163-ba70-4e7a-bbd2-31a2b3ebec77");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "589ec1fa-55fa-4291-959f-8f2c789d0512");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "8f612bce-8426-4a60-a616-eed1c57cb7a2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "691687e1-f005-449f-82aa-f61c1f38189b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "f775bef8-ae12-42e4-a180-0f52381dd8f6");
+
+            migrationBuilder.InsertData(
+                table: "equipement",
+                columns: new[] { "id", "mobile", "nom", "nomTabPcVue", "numGmao", "type_activites", "zoneID" },
+                values: new object[] { 268, false, "Pilote UF forte concentration", null, "PILOT0023", "8", 8 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 268);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "3f5ace32-e5a4-4766-8fda-1f586d6852cf");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "9382ac31-797a-42a0-9367-7c9fe0d41f58");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "7b923942-14e3-40b7-8547-a4681d28c889");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "14a53afe-5b81-4e69-a6fd-e640c951b3ea");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "8a75d158-0acd-4c98-b88a-6c1a3a5d46dc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "8d320402-6982-49ec-9f8a-8ce28040bdb8");
+        }
+    }
+}
