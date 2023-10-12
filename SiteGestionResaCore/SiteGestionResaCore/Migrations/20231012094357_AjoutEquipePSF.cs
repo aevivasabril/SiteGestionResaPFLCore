043b8071f55d026777b9788b65126f35cde085f0
VIVAS <anny.vivas@inra.fr>
2023-10-12 17:02:31 +0200

    Ajout de l'équipe PSF dans la base des données

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20231012094357_AjoutEquipePSF.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class AjoutEquipePSF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "794a363b-e780-4068-bbd3-36add15462c4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "b130cb64-a31b-4020-8d93-839d8354ee65");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "b2b350d7-217d-4fb1-8e95-513e76b12ff9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "91b160f9-af80-4ff5-b7d2-5030de680edb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "73cdb6dd-7190-4e10-b833-447e146b9edb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "380eaf19-ee9a-4599-b2c9-b1e609b802c2");
+
+            migrationBuilder.InsertData(
+                table: "ld_equipes_stlo",
+                columns: new[] { "id", "nom_equipe" },
+                values: new object[] { 8, "PSF" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ld_equipes_stlo",
+                keyColumn: "id",
+                keyValue: 8);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "146e0bd4-1099-46ef-97b6-59143850557a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "62850163-ba70-4e7a-bbd2-31a2b3ebec77");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "589ec1fa-55fa-4291-959f-8f2c789d0512");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "8f612bce-8426-4a60-a616-eed1c57cb7a2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "691687e1-f005-449f-82aa-f61c1f38189b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "f775bef8-ae12-42e4-a180-0f52381dd8f6");
+        }
+    }
+}
