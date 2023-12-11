c81a3f1497d2da0aeffca9a274a68ffec2cd0a7c
VIVAS <anny.vivas@inra.fr>
2023-12-11 10:45:37 +0100

    Oublié de mettre la migration -.-!

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20231211092156_PetitChangementECUV0007.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class PetitChangementECUV0007 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "5d289992-71dd-40be-b374-c57ac176c00b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "f3b08830-442e-4236-92a1-c5390f6ff9c3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "f3c2ff5a-e70b-4efd-9e73-245a654e87eb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "3d769575-727f-4cca-ab6b-03d5acb6a5a1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "178f65d3-84d6-44f2-a369-bef9d5e93bdc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "b0952767-eab6-406f-81c6-e7d898929e2f");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 187,
+                column: "nom",
+                value: "Mini cuve N°7 (150L)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "c527e101-7ce2-467c-bb7a-0d638d82f270");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "618e284e-fae7-4371-b32a-4f578823afa9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "6e2329be-87bf-4155-8967-9a84d6b847c2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "79add88e-243c-4d41-a353-ae14c609a67a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "c91a235a-b0e4-4d39-91e8-54961c985145");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "7e496a9f-3f06-4af7-9ee6-d5bc98ffb7ab");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 187,
+                column: "nom",
+                value: "Mini cuve 150L");
+        }
+    }
+}
