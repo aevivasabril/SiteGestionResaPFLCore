4508284999b9ef36f471674591abb5c4f6c3e230
VIVAS <anny.vivas@inra.fr>
2023-12-12 13:47:46 +0100

    Migration pour supprimer le pilote PILOT0009 Uf TAMI/tech-sep8 KDa (13m2) qui a été vendu

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20231212123739_suppressionPilote0009Vendu.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class suppressionPilote0009Vendu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 221);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "d0a84190-2265-43c4-8040-f5fd110949e1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "92916c72-7987-4ffe-a48d-fb9a6e2d6a06");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "68617b33-61b3-42de-8104-96ce5fdd9cc7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "e869bc2f-c159-449b-af4c-5efb64560bff");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "10b5ab22-e9a5-48e9-a3a3-f8fb1e26dd20");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "ce35c4c4-de0c-40ab-b03b-a755552e47c3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "5d289992-71dd-40be-b374-c57ac176c00b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "f3b08830-442e-4236-92a1-c5390f6ff9c3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "f3c2ff5a-e70b-4efd-9e73-245a654e87eb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "3d769575-727f-4cca-ab6b-03d5acb6a5a1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "178f65d3-84d6-44f2-a369-bef9d5e93bdc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "b0952767-eab6-406f-81c6-e7d898929e2f");
+
+            migrationBuilder.InsertData(
+                table: "equipement",
+                columns: new[] { "id", "mobile", "nom", "nomTabPcVue", "numGmao", "type_activites", "zoneID" },
+                values: new object[] { 221, false, "UF TAMI/tech-sep 8 kDa (13 m2)", null, "PILOT0009", "8", 8 });
+        }
+    }
+}
