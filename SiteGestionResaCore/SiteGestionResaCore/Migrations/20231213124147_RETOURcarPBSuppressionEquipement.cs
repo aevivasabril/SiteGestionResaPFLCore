6cf9730b0ffc083a8e7491bec783addc6883a3b9
VIVAS <anny.vivas@inra.fr>
2023-12-13 13:48:24 +0100

    VERSION 5.5.1 KO!!! je remets l'équipement dans la base des données car je peux pas l'effacer comme ça car s'il y a une réservation j'ai une exception

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20231213124147_RETOURcarPBSuppressionEquipement.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class RETOURcarPBSuppressionEquipement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "6026992d-b13b-448d-8f3a-5d97703f785c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "8ed67ac5-96c7-4360-91b5-4329a60504d2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "8bfd2285-5e92-4e18-9a28-a74d32ac445a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "b1464f41-f863-4f4c-9709-298416bab409");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "3d10c60f-286f-43e7-acc5-69883e37d3c6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "4768df8d-d603-468d-9689-c7025e687c94");
+
+            migrationBuilder.InsertData(
+                table: "equipement",
+                columns: new[] { "id", "mobile", "nom", "nomTabPcVue", "numGmao", "type_activites", "zoneID" },
+                values: new object[] { 221, false, "UF TAMI/tech-sep 8 kDa (13 m2)", null, "PILOT0009", "8", 8 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 221);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "d0a84190-2265-43c4-8040-f5fd110949e1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "92916c72-7987-4ffe-a48d-fb9a6e2d6a06");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "68617b33-61b3-42de-8104-96ce5fdd9cc7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "e869bc2f-c159-449b-af4c-5efb64560bff");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "10b5ab22-e9a5-48e9-a3a3-f8fb1e26dd20");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "ce35c4c4-de0c-40ab-b03b-a755552e47c3");
+        }
+    }
+}
