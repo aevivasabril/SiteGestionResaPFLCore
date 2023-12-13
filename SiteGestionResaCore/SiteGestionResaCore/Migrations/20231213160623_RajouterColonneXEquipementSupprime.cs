75de7d7eb80beeea939d73e13c51039ecfdb64a0
VIVAS <anny.vivas@inra.fr>
2023-12-13 17:08:56 +0100

    Rajout d'une colonne pour indiquer que l'équipement est supprimé ou enlevé de la PFL!

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20231213160623_RajouterColonneXEquipementSupprime.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class RajouterColonneXEquipementSupprime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "equip_delete",
+                table: "equipement",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "dfdd2f4d-f037-4e55-8e57-df4e73fa2a1b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "1cc72cbd-d09c-4fac-bc35-080fdefbb90d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "d7e3215e-cc7a-4a6a-8850-abcc9a3721ee");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "d3125179-ce18-41e3-8d26-ece34887bdde");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "29f3b4a6-8a4b-46fb-8d6b-c24cedcebcb5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "9b02a747-704f-4751-8945-d7953a3ab143");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "equip_delete",
+                table: "equipement");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "6026992d-b13b-448d-8f3a-5d97703f785c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "8ed67ac5-96c7-4360-91b5-4329a60504d2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "8bfd2285-5e92-4e18-9a28-a74d32ac445a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "b1464f41-f863-4f4c-9709-298416bab409");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "3d10c60f-286f-43e7-acc5-69883e37d3c6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "4768df8d-d603-468d-9689-c7025e687c94");
+        }
+    }
+}
