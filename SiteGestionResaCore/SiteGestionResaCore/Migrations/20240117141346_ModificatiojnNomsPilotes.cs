6042230fd67db9b835947288a75b087b18d28040
VIVAS <anny.vivas@inra.fr>
2024-01-17 15:16:16 +0100

    changement des noms pour le pilot0022 et pilot0013

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20240117141346_ModificatiojnNomsPilotes.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class ModificatiojnNomsPilotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "4e03bf23-9ec1-4183-8ae7-f0d712014fbe");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d6cce723-0af8-44a0-9cc3-12e58a98d3cf");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "4fa101b7-613c-4da0-9683-2ef252e3d544");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "fac24db9-c976-465e-83fe-3cf1d4873cc8");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "79b6a585-8aff-4d61-a42f-fe5efa01299b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "6951d63c-9b6b-42fb-a88a-aa7cde9d5235");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 224,
+                column: "nom",
+                value: "Pilote TIA air comprimé");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 243,
+                column: "nom",
+                value: "Boucle de Traitement Thermique");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "ad90df36-1746-4913-8a0f-a6cdc1b0280b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "abaa7cdd-76c0-4fd4-bd09-f47ad4f0bdd0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "1ca09524-fdff-4df0-ab64-8d70c8fa8d8d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "e39a1d30-0f83-4cab-ac8f-278c4b7d837c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "913dca53-5eb1-4951-ad64-11c622dfd749");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "7762ffe3-68a2-40f4-b231-4dcd0dc237ff");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 224,
+                column: "nom",
+                value: "Pilote UF TAMI/Tia 8Kda mobile");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 243,
+                column: "nom",
+                value: "Boucle de Traitement Thermique Bain-marie MEMMERT - Type WNE45 + Thermo Haake K35");
+        }
+    }
+}
