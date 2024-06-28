f1a90429c9110b29d7df573a3289ab2a129ac4ab
VIVAS <anny.vivas@inrae.fr>
2024-06-28 11:01:25 +0200

    Changement Nom Stephan pour bien tester si mon nouvel ordi créé bien le site à installer: tout semble fonctionner correctement.. A voir avec l'utilisation

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20240628083305_ChangementNomStephanTESTNvoOrdi.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class ChangementNomStephanTESTNvoOrdi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "7a1d03bb-4e0d-42f4-a84b-33a46c3b626a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "f21b105a-a451-4c6c-96ec-a6dbb7f3d059");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "8aa28ffe-e733-4fa5-ba32-a6acb73d2e44");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "d2ba7649-019b-40ff-ac72-dcae631b5f32");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "69221ee4-d15e-4ff8-8c4e-4a8e2be57693");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "00628428-374a-4c1a-b7e1-2cb7270c80d5");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 170,
+                column: "nom",
+                value: "Mélangeur cuiseur Stephan");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 268,
+                column: "nomTabPcVue",
+                value: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "db906827-d001-40aa-83e9-4d8155552760");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "ea5f133c-37a7-4011-9ba4-951acd7bc1a9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "3f31a42c-e21c-402f-8cba-f6e6ec418130");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "704f814b-6256-41dd-bc65-4be1675c2725");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "1c576c3d-b704-4e26-8eb1-e58ca84435cc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "681872b3-06f6-444b-aebd-2149303f07cc");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 170,
+                column: "nom",
+                value: "Mélangeur cuiseur stéphan");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 268,
+                column: "nomTabPcVue",
+                value: "tab_UA_UFFC");
+        }
+    }
+}
