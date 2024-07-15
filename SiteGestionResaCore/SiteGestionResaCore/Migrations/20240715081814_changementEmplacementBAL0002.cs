7dfc67c98adc87edb91a9df53c66e97e8ec5b7bf
VIVAS <anny.vivas@inrae.fr>
2024-07-15 10:25:12 +0200

    Changement d'emplacement pour la BAL0002

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20240715081814_changementEmplacementBAL0002.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class changementEmplacementBAL0002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "ffd7539d-02aa-4fc4-93fd-6740c4898aa6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d5d93965-5410-45eb-b756-d047206157bb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "b3581acd-ed67-4c38-8296-41daf1fb323c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "cbe30f72-e999-4f79-95eb-a65ea61d189b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "d25bf70c-0195-4402-9530-3720367821b4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "01792748-5f1d-4727-826c-8f6f909c2969");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 162,
+                columns: new[] { "nom", "zoneID" },
+                values: new object[] { "Balance Arpège 150kg", 6 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "5f9001b5-987a-44e7-99f9-1ac56c3454c9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "945ca5db-dc0b-4ac9-a0e1-d16c2690f9ef");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "d509125a-a0c7-45c3-99db-228538be7b31");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "98d0590d-c182-40a3-bac5-92dec5671a93");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "6bb6d919-7d9b-4fa7-9303-2d9b709ec9d1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "5320d765-b0d0-43a1-8b1d-fa0297c650e3");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 162,
+                columns: new[] { "nom", "zoneID" },
+                values: new object[] { "Balance Arpège 150k", 5 });
+        }
+    }
+}
