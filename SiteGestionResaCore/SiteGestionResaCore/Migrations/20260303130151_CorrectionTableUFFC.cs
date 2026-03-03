934303e602e6c232c175c21324d1c13ef775ffb1
Anny VIVAS <anny.vivas@inrae.fr>
2026-03-03 14:34:34 +0100

    Correction car perte du nom de la table pour le pilote UFFC qui normalement doit pouvoir récupérer des données mais ce n'était pas le cas, ce bug trainait depuis le merge du 15/03/2024!

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20260303130151_CorrectionTableUFFC.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class CorrectionTableUFFC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "e10acce7-ff1f-4958-9c58-c260adb39c4d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "08bf8674-f958-42d9-bc0a-c4bb1497959c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "b67b4085-a6b0-4b53-aace-968c4a4b8433");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "05731273-0c4a-4a9a-ba92-6f8f5b41ece6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "8b133726-4ffc-4c00-8bad-7dc742605d31");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "656d4b65-733d-43c6-9d8a-40452ff75e83");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 268,
+                column: "nomTabPcVue",
+                value: "tab_UA_UFFC");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "22f11612-8200-4c84-a684-2d36c453036a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "995bd1f2-7e6e-4a7f-980b-4fb81772934a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "dbf0a19e-42d7-4c13-ac01-7a94fdbeb36c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "23a67b3a-be9f-48e6-8f00-67d7f23e8ea5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "caa36eed-8225-4090-ae3d-bc5cf0197e5b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "6d7c1149-605f-461c-92c9-76c864a62b9a");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 268,
+                column: "nomTabPcVue",
+                value: null);
+        }
+    }
+}
