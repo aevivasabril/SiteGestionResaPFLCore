688f163dde8132fb039d0c44be07ccdb7f9e3c48
Anny VIVAS <anny.vivas@inrae.fr>
2026-05-07 11:32:17 +0200

    Ajout du nom de la table des données PcVue pour le cuiseur Stephan

---
+++ SiteGestionResaCore/SiteGestionResaCore/Migrations/20260507092853_AjoutNomTableStephan.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteGestionResaCore.Migrations
+{
+    public partial class AjoutNomTableStephan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "e1ae64c1-917b-46d9-96c9-0122626767fb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "e2ea4a47-41ba-4b14-8d5c-3fb2ae768473");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "8a4bcf72-8e5c-4b8b-8c6a-d7b8cc1d4c7d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "4f17df1b-1057-4707-be32-60348bcad5f1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "0b109fbc-9bdb-4c7d-86eb-6f4898aa1c81");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "1e0e6d90-1d88-4903-8824-7ff998d0adfd");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 170,
+                column: "nomTabPcVue",
+                value: "tab_UA_STEPHAN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "841f80e1-39e4-47be-88ad-d7739a671d84");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "718cfdaa-fa01-4565-9ea3-4439635349c5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "7a980248-43a5-4e69-ac3a-727dca2d9d80");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "1465583b-a03f-482c-879d-748134717640");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "086b0371-075c-40bf-866e-13245ec56086");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "a9f6b385-4cd4-4943-961d-a28960ec7494");
+
+            migrationBuilder.UpdateData(
+                table: "equipement",
+                keyColumn: "id",
+                keyValue: 170,
+                column: "nomTabPcVue",
+                value: null);
+        }
+    }
+}
